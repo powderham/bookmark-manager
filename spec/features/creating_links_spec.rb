ef08d849039553aa740997998829ef2bc83ae8c0
your name <akram.rasikh@gmail.com>
2016-10-19 11:58:47 +0100

    now question 12

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -3,15 +3,13 @@ require './models/link'
 
 feature 'add new links to website', :type => feature do
   scenario "adds links" do
-    visit '/links'
+    visit '/links/new'
+    fill_in "title", with: 'this is google'
     fill_in 'url', with: 'http://www.google.com'
-    fill_in 'title', with: 'this is google'
-    click_button "Submit"
-
+    click_button "Create link"
     expect(current_path).to eq '/links'
-
-    within 'ul#links' do
+    # within 'ul#links' do
       expect(page).to have_text('this is google')
-    end
+    # end
   end
 end
