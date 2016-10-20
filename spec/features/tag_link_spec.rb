458bdb1287ddfe56d82a0fce48cfc76dbe80fbec
Ed Powderham <edpowderham@outlook.com>
2016-10-20 14:40:10 +0100

    Update tag link status

--- spec/features/tag_link_spec.rb
+++ spec/features/tag_link_spec.rb
@@ -5,12 +5,10 @@ require 'database_cleaner'
 RSpec.feature "tag link", :type => :feature do
 
   scenario "tagging links" do
-    # Link.create(url: 'https://www.bbc.co.uk/', title: 'BBC', tag: 'news')
     visit '/new'
     fill_in :title, with: 'BBC'
     fill_in :url, with: 'https://www.bbc.co.uk/'
     fill_in :tag, with: 'news'
-
     click_button "Create link"
     expect(page).to have_text "news"
   end
